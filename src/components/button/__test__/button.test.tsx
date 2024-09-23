2f306cce530889dee29339785237fe926c8188a2
Isaac Sneddon-Jenkins <info.isaacsneddon@gmail.com>
2024-09-23 16:35:48 +0100

    fix: test import on button incorrect

--- src/components/button/__test__/button.test.tsx
+++ src/components/button/__test__/button.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Button from "../Button";
+import Button from "../button";
 
 describe("Button component", () => {
   it("Button should render correctly", () => {
